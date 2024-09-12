aa3691f2942f1925538864d9853bedaa4c9bfce3
singularapp <terrence@singulardex.com>
2024-09-12 16:31:43 -0400

    add LaunchPad page

--- src/App/AppRoutes.tsx
+++ src/App/AppRoutes.tsx
@@ -30,6 +30,7 @@ import { useConfigureMetrics } from "lib/metrics/useConfigureMetrics";
 import { BuyCryptoModal } from "components/BuyCrypto/BuyCryptoModal";
 import { GateFiDisplayModeEnum, GateFiSDK } from "@gatefi/js-sdk";
 import { randomHash } from "lib/crypto";
+import classNames from "classnames";
 
 const Zoom = cssTransition({
   enter: "zoomIn",
@@ -150,7 +151,7 @@ export function AppRoutes() {
   return (
     <>
       <div className="App">
-        <div className="App-content">
+        <div className={classNames("App-content", { launchpad: location.pathname.includes("/launchpad") })}>
           <Header
             disconnectAccountAndCloseSettings={disconnectAccountAndCloseSettings}
             openSettings={openSettings}
